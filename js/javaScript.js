8e997ebc2ae2c4f85b9f1df840661494a6dd51ed
Adrian Del Moral <iFullStart@gmail.com>
2021-10-01 15:36:14 +0200

    v.2 Va todo bien ya, y comprobado

--- js/javaScript.js
+++ js/javaScript.js
@@ -12,48 +12,108 @@ alert('Bienvenido/a a la calculadora de JavaScript');
             switch (opcion) {
                 case "+":
                 // Sumar
-                alert('Has seleccionado la opción Sumar.\n');    
-                nums = prompt('Introduce dos numeros separados por un espacio');
-                // Quitamos los espacios de mas, y los cambiamos por solo 1 espacio
-                nums = nums.replace(/\s+/g, " ");
-            
-                split2 = nums.split(' ') // (1) [ num1, num1 ]
-                num1 = split2[0] // (2) num1
-                num2 = split2[1] // (3) num2
-                            
-                // comprobar el punto 3
-                    // Not a Number // Nulo // Tiene letra
-                        if(!NaN && !null){
-                                num1 = parseInt(num1);
-                                num2 = parseInt(num2);
-                                alert(`El resultado de la suma es ${num1+num2}`);
-                                result = confirm("Quieres hacer otra operación?");
-                                if(result === false){
-                                    condition = false;
+                    alert('Has seleccionado la opción Sumar.\n');    
+                    nums = prompt('Introduce dos numeros separados por un espacio');
+                    // Quitamos los espacios de mas, y los cambiamos por solo 1 espacio
+                    // \s se refiere a cualquier símbolo de espacio en blanco: espacios, tabulaciones y saltos de línea.
+                    nums = nums.replace(/\s+/g, " ");
+                
+                    split2 = nums.split(' ') // (1) [ num1, num1 ]
+                    num1 = split2[0] // (2) num1
+                    num2 = split2[1] // (3) num2
+                                
+                    // Comprobar que son numeros solo y no contienen letras
+                            if(isNaN(num1) === false || isNaN(num2) === false){
+                                    num1 = parseInt(num1);
+                                    num2 = parseInt(num2);
+                                    alert(`El resultado de la Suma es: ${num1+num2}`);
+                                    result = confirm("Quieres hacer otra operación?");
+                                    if(result === false){
+                                        condition = false;
+                                    }
+                                }else{
+                                    alert("Error tienen que ser numeros, numeros y no numeros y letras!");
                                 }
-                            }else{
-                                alert("Error!");
-                            }
                 break;
 
                 case "-":
                 // Restar
-
+                    alert('Has seleccionado la opción Sumar.\n');    
+                    nums = prompt('Introduce dos numeros separados por un espacio');
+                    // Quitamos los espacios de mas, y los cambiamos por solo 1 espacio
+                    // \s se refiere a cualquier símbolo de espacio en blanco: espacios, tabulaciones y saltos de línea.
+                    nums = nums.replace(/\s+/g, " ");
+                
+                    split2 = nums.split(' ') // (1) [ num1, num2 ]
+                    num1 = split2[0] // (2) num1
+                    num2 = split2[1] // (3) num2
+                                
+                    // Comprobar que son numeros solo y no contienen letras
+                            if(isNaN(num1) === false || isNaN(num2) === false){
+                                    num1 = parseInt(num1);
+                                    num2 = parseInt(num2);
+                                    alert(`El resultado de la Resta es: ${num1-num2}`);
+                                    result = confirm("Quieres hacer otra operación?");
+                                    if(result === false){
+                                        condition = false;
+                                    }
+                                }else{
+                                    alert("Error tienen que ser numeros, numeros y no numeros y letras!");
+                                }
                 break;
                 case "*":
                 // Multipicar
-    
+                    alert('Has seleccionado la opción Sumar.\n');    
+                    nums = prompt('Introduce dos numeros separados por un espacio');
+                    // Quitamos los espacios de mas, y los cambiamos por solo 1 espacio
+                    // \s se refiere a cualquier símbolo de espacio en blanco: espacios, tabulaciones y saltos de línea.
+                    nums = nums.replace(/\s+/g, " ");
+                
+                    split2 = nums.split(' ') // (1) [ num1, num1 ]
+                    num1 = split2[0] // (2) num1
+                    num2 = split2[1] // (3) num2
+                                
+                    // Comprobar que son numeros y no tienen contienen letras
+                            if(isNaN(num1) === false || isNaN(num2) === false){
+                                    num1 = parseInt(num1);
+                                    num2 = parseInt(num2);
+                                    alert(`El resultado de la Multiplicación es: ${num1*num2}`);
+                                    result = confirm("Quieres hacer otra operación?");
+                                    if(result === false){
+                                        condition = false;
+                                    }
+                                }else{
+                                    alert("Error tienen que ser numeros, numeros y no numeros y letras!");
+                                }
                 break;
                 case "/":
-                            // Dividir
-    
-                break;
-                default:
-                    alert("Error dentro del switch!");
+                    // Dividir
+                        alert('Has seleccionado la opción Sumar.\n');    
+                        nums = prompt('Introduce dos numeros separados por un espacio');
+                        // Quitamos los espacios de mas, y los cambiamos por solo 1 espacio
+                        // \s se refiere a cualquier símbolo de espacio en blanco: espacios, tabulaciones y saltos de línea.
+                        nums = nums.replace(/\s+/g, " ");
+                        
+                        split2 = nums.split(' ') // (1) [ num1, num1 ]
+                        num1 = split2[0] // (2) num1
+                        num2 = split2[1] // (3) num2
+                                        
+                        // Comprobar que son numeros y no tienen contienen letras
+                            if(isNaN(num1) === false || isNaN(num2) === false){
+                                num1 = parseInt(num1);
+                                num2 = parseInt(num2);
+                                alert(`El resultado de la División es: ${num1/num2}`);
+                                result = confirm("Quieres hacer otra operación?");
+                                if(result === false){
+                                condition = false;
+                                }
+                            }else{
+                                alert("Error tienen que ser numeros, numeros y no numeros y letras!");
+                            }
                 break;
             }    
         } else{
-            alert("Error en else!");
-    }
+            alert("La operación introducida es erronea, Prueba de nuevo con una que esté indicada 🙄");
+        }
+        alert("Hasta la proxima 😄!");
 } while(contition === true);
-alert("Has salido de el bucle de opciones, error en do while");
